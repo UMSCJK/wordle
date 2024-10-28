51fc6d2fde86b46c3d26e8c86170ded6c8c15011
UMSC_Official <cao_junkai@qq.com>
2024-10-29 01:09:20 +0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # wordle
-A simple wordle game based on ExpressJS
+
+后端基于ExpressJS的全栈Wordle游戏
+
+## 中间件
+
+### 判断单词是否存在
+
+- 路由：GET `/api/exist` (Query)
+- 示例请求URL
+  - <http://127.0.0.1:8080/api/exist?lan=ru&wrd=тест>
+  - <http://127.0.0.1:8080/api/exist?lan=en&wrd=chinglish>
+
+### 对比单词
+
+- 路由：GET `/api/compare` (Query)
+- 示例请求URL：
+  - <http://127.0.0.1:8080/api/compare?a=кино&g=кофе>
+
+接受两个query参数`a`和`i`，分别为答案和猜测，传回对比结果对象。
+
+`result`键的值为一个数组，表示每个字母的对比结果，支持三种值，分别表示：
+
+| 值      | 意义                     |
+| ------- | ------------------------ |
+| `true`  | 对应字母存在，且位置正确 |
+| `false` | 对应字母存在，但位置不对 |
+| `null`  | 对应字母在原单词中不存在 |
+
+#### 示例
+
+- 完全正确：`кино`, `кино`
+
+```json
+{ // 返回数据
+	"same": true, // 是否相同
+	"input": ["кино", "кино"], // 答案、猜测
+	"result": [true, true, true, true]
+}
+```
+
+- 一般情况：`море`, `роса`
+
+```json
+{ // 返回数据
+	"same": false, // 是否相同
+	"input": ["море", "роса"], // 答案、猜测
+	"result": [false, true, null, null]
+}
+```
+
+- 单词长度不一样：`море`, `океан`
+
+```json
+{ // 返回数据
+	"info": "Different lengths" // 报错信息
+}
+```
+
+### 查找单词
+
+- 路由：POST `/api/find` (Body)
+- 示例Ajax请求：
+```js
+const conditions = []
+$.ajax({
+	url: '/api/find',
+	type: 'POST',
+	contentType: 'application/json; charset=utf-8',
+	data: JSON.stringify(conditions),
+	success: data => console.log(data)
+})
+```
--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-const [express, path, cors] = [ // 引入模块
-	require('express'),
-	require('path'),
-	require('cors')
-]
-const PORT = 8080 // 设置监听端口
-const app = express() // 声明app
+const fs = require('fs'), // 导入模块
+	path = require('path'),
+	cors = require('cors'),
+	express = require('express')
 
-app.use(cors(), express.json())
-app.use(express.static(path.join(__dirname, 'static')))
+const PORT = 8080, // 常量
+	app = express(),
+	db = { en: 'en.json', ru: 'ru.json' }
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+for (let lan in db) {
+	const filePath = path.join(__dirname, 'db', db[lan]),
+		str = fs.readFileSync(filePath, 'utf-8'),
+		dict = JSON.parse(str)
+	db[lan] = {}
+	for (let len in dict) {
+		const re = new RegExp(`.{${Number(len)}}`, 'g')
+		db[lan][len] = dict[len].toLowerCase().match(re) || []
+	}
+}
+
+// DEBUG: Write db into db.json
+// fs.writeFileSync('db.json', JSON.stringify(db, null, 4))
+
+// Route list
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/exist', (req, res) => { // 检测单词是否存在
+	const { lan, wrd } = req.query
+	const len = wrd.length.toString()
+	res.send(db[lan][len].includes(wrd))
+})
+
+app.get('/api/compare', (req, res) => { // 对比两个单词
+	const a = req.query.a.toLowerCase()
+	const g = req.query.g.toLowerCase()
+	if (a.length !== g.length) { // 错误处理：若长度不一样
+		res.json({ info: "Different lengths" })
+	} else {
+		let result = new Array(a.length).fill(undefined)
+		for (let i = 0; i < g.length; i++) {
+			if (a[i] === g[i]) {
+				result[i] = true
+			} else if (a.includes(g[i])) {
+				result[i] = false
+			} else {
+				result[i] = null
+			}
+		}
+		res.json({
+			same: a === g,
+			input: [a, g],
+			result: result
+		})
+	}
+})
+
+app.post('/api/find', (req, res) => { // 返回符合条件的词表
+	// TODO: 使用req.body，先写文档
+	// https://wordfinder.co/wordle-solver
 })
 
 app.listen(PORT, () => {
